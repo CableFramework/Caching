f61e3769750263394433e25abec30065e188510c
anonymframework <vahit.serif119@gmail.com>
2017-05-29 19:24:19 +0300

    made some updates

---
+++ src/Components/Caching/Cache.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Cable\Components\Caching;
+
+
+use Cable\Container\ContainerInterface;
+
+class Cache
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+
+    protected $drivers = [
+
+    ];
+
+    /**
+     * @var array
+     */
+    private $configs = [];
+
+    /**
+     * @var string
+     */
+    protected $defaultDriver;
+
+    /**
+     * Cache constructor.
+     * @param ContainerInterface $container
+     * @param array $configs
+     */
+    public function __construct(ContainerInterface $container, array $configs = [])
+    {
+        $this->container = $container;
+
+        $this->dispatchConfigs($configs);
+    }
+
+    /**
+     * @param array $configs
+     * @return void returns nothings
+     */
+    private function dispatchConfigs(array $configs)
+    {
+        if (isset($configs['default'])) {
+            $this->defaultDriver = $configs['default'];
+            unset($configs['default']);
+        }
+
+        if (isset($configs['drivers'])) {
+            $this->drivers = $configs['drivers'];
+            unset($configs['drivers']);
+        }
+
+        $this->configs = $configs;
+    }
+
+}
