9701d01d5975589938cab2fede3b8d558647bdc0
anonymframework <vahit.serif119@gmail.com>
2017-05-29 19:19:52 +0300

    added not timeable driver interface

--- src/Components/Caching/Driver/NotTimeableDriverInterface.php
+++ src/Components/Caching/Driver/NotTimeableDriverInterface.php
@@ -1,9 +1,17 @@
 <?php
 namespace Cable\Components\Caching\Driver;
 
-
+/**
+ * Interface NotTimeableDriverInterface
+ * @package Cable\Components\Caching\Driver
+ */
 interface NotTimeableDriverInterface
 {
 
-
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return mixed
+     */
+    public function set($name, $value);
 }
