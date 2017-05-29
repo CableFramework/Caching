bba235ff8649b8991414eda2874ec9838b54c36f
anonymframework <vahit.serif119@gmail.com>
2017-05-29 20:27:50 +0300

    added compressor interface

--- src/Components/Caching/Compressor/CompressorInterface.php
+++ src/Components/Caching/Compressor/CompressorInterface.php
@@ -2,8 +2,22 @@
 
 namespace Cable\Components\Caching\Compressor;
 
-
+/**
+ * Interface CompressorInterface
+ * @package Cable\Components\Caching\Compressor
+ */
 interface CompressorInterface
 {
 
+    /**
+     * @param mixed $data
+     * @return string
+     */
+    public function compress($data);
+
+    /**
+     * @param string $data
+     * @return mixed
+     */
+    public function uncompress($data);
 }
